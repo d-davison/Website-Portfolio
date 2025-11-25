4da4fe9c106c230960b6e4aa73e1b1fc01e13e3a
Dan Davison <dandavison99@gmail.com>
2025-11-25 22:35:27 +0000

    WIP of new pages for Night Crew and Cruise Murder Mystery

--- assets/components/featureBox.js
+++ assets/components/featureBox.js
@@ -6,7 +6,7 @@ class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
       <div class="col-12 col-md-6 col-lg-4 item">
-      <a href="page-game-jostl.html">
+      <a href="jostl.html">
           <div class="card featured">
               <h4>JOSTL.</h4>
               <p style="color:#757575">A 3rd Screen Sports for Android and iOS</p>
--- assets/components/navbar-items.js
+++ assets/components/navbar-items.js
@@ -10,7 +10,7 @@ class NavbarItems extends HTMLElement {
           <a href="#" class="nav-link">Projects<i class="icon-arrow-down"></i></a>
           <ul class="dropdown-menu">
               <li class="nav-item"><a href="page-game-.html" class="nav-link">VR & Chronic Pain</a></li>
-              <li class="nav-item"><a href="page-game-jostl.html" class="nav-link">JOSTL.</a></li>
+              <li class="nav-item"><a href="jostl.html" class="nav-link">JOSTL.</a></li>
               <li class="nav-item"><a href="page-game-tiltrocket.html" class="nav-link">Tilt Rocket</a></li>
           </ul>
       </li>      
